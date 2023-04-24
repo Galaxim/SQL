a82c1b79f367f598133e1e029f305581e9bb0d51
Kristian <asdf>
2023-04-24 13:12:50 +0200

    sql

---
+++ sql.java
@@ -0,0 +1,14 @@
+public class sql {
+    "alter table ingredient\n" +
+    "modify gluten_free boolean not null;\n" +
+    "alter table ingredient\n" +
+    "modify lactose_free boolean not null;\n" +
+    "alter table ingredient\n" +
+    "modify price double not null;\n" +
+    "alter table ingredient\n" +
+    "modify vegetarian boolean not null;\n" +
+    "alter table ingredient\n" +
+    "modify vegan boolean not null;\n" +
+    "alter table ingredient\n" +
+    "modify name varchar(255) not null;\n";
+}
